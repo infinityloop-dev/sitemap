f145f586e20db6520f3cf8e212c71f0d9049923a
Václav Pelíšek <peldax@gmail.com>
2018-04-17 13:25:12 +0200

    Update TSitemap.php

--- src/TI/TSitemap.php
+++ src/TI/TSitemap.php
@@ -30,20 +30,22 @@ trait TSitemap
                 continue;
             }
 
-            if ($method->hasAnnotation('sitemap'))
+            if (!$method->hasAnnotation('sitemap'))
             {
-                $regex = '/App\\\\([A-Z][a-z]*)Module\\\\Presenter\\\\([A-Z][a-z]*)Presenter/';
-                $matches = [];
-
-                preg_match($regex, $reflection->name, $matches);
-
-                if (\count($matches) < 3)
-                {
-                    continue;
-                }
-
-                $pages[] = ":{$matches[1]}:{$matches[2]}:" . lcfirst(substr($method->name, 6));
+                continue;
             }
+            
+            $regex = '/App\\\\([A-Z][a-z]*)Module\\\\Presenter\\\\([A-Z][a-z]*)Presenter/';
+            $matches = [];
+
+            preg_match($regex, $reflection->name, $matches);
+
+            if (\count($matches) < 3)
+            {
+                continue;
+            }
+
+            $pages[] = ":{$matches[1]}:{$matches[2]}:" . lcfirst(substr($method->name, 6));
         }
 
         return $pages;
