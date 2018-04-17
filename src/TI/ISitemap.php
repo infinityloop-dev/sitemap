f45bd89961be9cb923460980ee7a67e2f755b51c
Václav Pelíšek <peldax@gmail.com>
2018-04-17 13:11:43 +0200

    Create ISitemap.php

---
+++ src/TI/ISitemap.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of Nepttune (https://www.peldax.com)
+ *
+ * Copyright (c) 2018 Václav Pelíšek (info@peldax.com)
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <https://www.peldax.com>.
+ */
+
+declare(strict_types = 1);
+
+namespace Nepttune\TI;
+
+interface ISitemap
+{
+    public function getSitemap();
+}
