96aeb3e3dbade42c28eadb5f766009f959d1c27a
Václav Pelíšek <peldax@gmail.com>
2018-04-25 14:40:12 +0200

    Update TSitemap.php

--- src/TI/TSitemap.php
+++ src/TI/TSitemap.php
@@ -25,27 +25,14 @@ trait TSitemap
 
         foreach ($reflection->getMethods(\ReflectionMethod::IS_PUBLIC) as $method)
         {
-            if ($method->class !== $reflection->getName() || substr($method->name, 0, 6) !== 'action')
+            if ($method->class !== $reflection->getName() ||
+                substr($method->name, 0, 6) !== 'action' ||
+                !$method->hasAnnotation('sitemap'))
             {
                 continue;
             }
 
-            if (!$method->hasAnnotation('sitemap'))
-            {
-                continue;
-            }
-            
-            $regex = '/App\\\\([A-Z][a-z]*)Module\\\\Presenter\\\\([A-Z][a-z]*)Presenter/';
-            $matches = [];
-
-            preg_match($regex, $reflection->name, $matches);
-
-            if (\count($matches) < 3)
-            {
-                continue;
-            }
-
-            $pages[] = ":{$matches[1]}:{$matches[2]}:" . lcfirst(substr($method->name, 6));
+            $pages[] = ":{$this->getName()}:" . lcfirst(substr($method->name, 6));
         }
 
         return $pages;
