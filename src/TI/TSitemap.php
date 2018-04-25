f2ee30693420fe3f2fe05798206a0ed0bb20797a
Václav Pelíšek <peldax@gmail.com>
2018-04-25 15:05:58 +0200

    Update TSitemap.php

--- src/TI/TSitemap.php
+++ src/TI/TSitemap.php
@@ -16,7 +16,7 @@ namespace Nepttune\TI;
 
 trait TSitemap
 {
-    public function getSitemap() : array
+    public static function getSitemap() : array
     {
         $pages = [];
 
@@ -32,7 +32,16 @@ trait TSitemap
                 continue;
             }
 
-            $pages[] = ":{$this->getName()}:" . lcfirst(substr($method->name, 6));
+            $regex = '/App\\\\([A-Z][a-z]*)Module\\\\Presenter\\\\([A-Z][a-z]*)Presenter/';
+            $matches = [];
+            preg_match($regex, $reflection->name, $matches);
+
+            if (\count($matches) < 3)
+            {
+                continue;
+            }
+
+            $pages[] = ":{$matches[1]}:{$matches[2]}:" . lcfirst(substr($method->name, 6));
         }
 
         return $pages;
